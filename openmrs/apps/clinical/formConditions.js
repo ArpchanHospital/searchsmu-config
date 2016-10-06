3409b36653441700b6dbcd85438e07f14b57e2fd
rdeolal <rdeolal@inrdeolal.local>
2016-10-06 18:06:08 +0530

    RaviDeolal : Removed the Random blood sugar related form conditions

--- openmrs/apps/clinical/formConditions.js
+++ openmrs/apps/clinical/formConditions.js
@@ -27,19 +27,6 @@ Bahmni.ConceptSet.FormConditions.rules = {
             }
         }
     },
-    'Random Blood Sugar Data' : function (formName, formFieldValues) {
-        var systolic = formFieldValues['Systolic'];
-        var diastolic = formFieldValues['Diastolic'];
-        if (systolic || diastolic) {
-            return {
-                enable: ["Posture"]
-            }
-        } else {
-            return {
-                disable: ["Posture"]
-            }
-        }
-    },
     'DM Follow, ADRs' : function (formName, formFieldValues) {
         var result = formFieldValues['DM Follow, ADRs'];
         if (result == "SAE") {
