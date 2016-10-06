cbe416f07273f1c5da8487f49798ee75bb3c5722
rdeolal <rdeolal@inrdeolal.local>
2016-10-06 15:54:31 +0530

    RaviDeolal : Added the form condition to handle the SAE enabling of the textbox

--- openmrs/apps/clinical/formConditions.js
+++ openmrs/apps/clinical/formConditions.js
@@ -2,8 +2,7 @@ Bahmni.ConceptSet.FormConditions.rules = {
     'Diastolic Data' : function (formName, formFieldValues) {
         var systolic = formFieldValues['Systolic'];
         var diastolic = formFieldValues['Diastolic'];
-        var rbs = formFieldValues['Random Blood Sugar'];
-        if (systolic || diastolic || rbs) {
+        if (systolic || diastolic) {
 
             return {
                 enable: ["Posture"]
@@ -17,8 +16,7 @@ Bahmni.ConceptSet.FormConditions.rules = {
     'Systolic Data' : function (formName, formFieldValues) {
         var systolic = formFieldValues['Systolic'];
         var diastolic = formFieldValues['Diastolic'];
-        var rbs = formFieldValues['Random Blood Sugar'];
-        if (systolic || diastolic || rbs) {
+        if (systolic || diastolic) {
 
             return {
                 enable: ["Posture"]
@@ -32,14 +30,49 @@ Bahmni.ConceptSet.FormConditions.rules = {
     'Random Blood Sugar Data' : function (formName, formFieldValues) {
         var systolic = formFieldValues['Systolic'];
         var diastolic = formFieldValues['Diastolic'];
-        var rbs = formFieldValues['Random Blood Sugar'];
-        if (systolic || diastolic || rbs) {
+        if (systolic || diastolic) {
             return {
                 enable: ["Posture"]
             }
         } else {
             return {
                 disable: ["Posture"]
+            }
+        }
+    },
+    'DM Follow, ADRs' : function (formName, formFieldValues) {
+        var result = formFieldValues['DM Follow, ADRs'];
+        if (result == "SAE") {
+            return {
+                enable: ["DM Follow, SAE description"]
+            }
+        } else {
+            return {
+                disable: ["DM Follow, SAE description"]
+            }
+        }
+    },
+    'HTN Follow, ADRs' : function (formName, formFieldValues) {
+        var result = formFieldValues['HTN Follow, ADRs'];
+        if (result == "SAE") {
+            return {
+                enable: ["HTN Follow, SAE description"]
+            }
+        } else {
+            return {
+                disable: ["HTN Follow, SAE description"]
+            }
+        }
+    },
+    'Stroke Follow, ADRs' : function (formName, formFieldValues) {
+        var result = formFieldValues['Stroke Follow, ADRs'];
+        if (result == "SAE") {
+            return {
+                enable: ["Stroke Follow, SAE description"]
+            }
+        } else {
+            return {
+                disable: ["Stroke Follow, SAE description"]
             }
         }
     }
